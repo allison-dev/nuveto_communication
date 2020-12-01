b2847d52337ccbce8d5a5a9aedde39946663c5eb
Allison Oliveira <aoliveira@nuveto.com.br>
2020-11-30 23:45:23 -0300

    composer lock

--- config/app.php
+++ config/app.php
@@ -175,7 +175,6 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\TelescopeServiceProvider::class,
-        Chatify\ChatifyServiceProvider::class,
         App\Providers\TelescopeServiceProvider::class,
 
     ],
