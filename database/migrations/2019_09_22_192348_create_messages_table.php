22992cd1a23377d8ee7211ce2ba1329638dabc51
Allison Oliveira <aoliveira@nuveto.com.br>
2020-12-03 17:52:59 -0300

    changes

--- database/migrations/2019_09_22_192348_create_messages_table.php
+++ database/migrations/2019_09_22_192348_create_messages_table.php
@@ -14,15 +14,15 @@ class CreateMessagesTable extends Migration
     public function up()
     {
         Schema::create('messages', function (Blueprint $table) {
-            $table->bigInteger('id');
+            $table->string('id');
             $table->string('type');
-            $table->bigInteger('from_id');
-            $table->bigInteger('to_id');
+            $table->string('from_id');
+            $table->string('to_id');
             $table->string('body',5000)->nullable();
             $table->string('attachment')->nullable();
             $table->boolean('seen')->default(false);
             $table->timestamps();
-            
+
             $table->primary('id');
         });
     }
