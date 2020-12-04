61c53794f251ccff7e685b9d929396bf6e7fd9d5
Allison Oliveira <aoliveira@nuveto.com.br>
2020-12-04 17:37:35 -0300

    changes

--- app/Http/Controllers/fivenineCallbackController.php
+++ app/Http/Controllers/fivenineCallbackController.php
@@ -9,18 +9,16 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
-require_once 'vendor/autoload.php';
-
 class fivenineCallbackController extends Controller
 {
     public function messageCreateCallback(Request $request)
     {
-        $faker = Faker\Factory::create('pt_BR');
+        // $faker = Faker\Factory::create('pt_BR');
 
         $insert_params = [
             'id'                => $request['correlationId'],
-            'name'              => $faker->name,
-            'email'             => $faker->unique()->safeEmail,
+            'name'              => 'teste API',
+            'email'             => 'teste@tes.com',
             'email_verified_at' => now(),
             'password'          => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token'    => Str::random(10),
