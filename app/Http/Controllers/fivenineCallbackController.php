e617896ed9dd3e600df505b1c0f67088374a6dc3
Allison Oliveira <aoliveira@nuveto.com.br>
2020-12-04 18:18:50 -0300

    changes

--- app/Http/Controllers/fivenineCallbackController.php
+++ app/Http/Controllers/fivenineCallbackController.php
@@ -16,11 +16,11 @@ class fivenineCallbackController extends Controller
         // $faker = Faker\Factory::create('pt_BR');
 
         $insert_params = [
-            'id'                => $request['correlationId'],
             'name'              => 'teste API',
             'email'             => 'teste@tes.com',
             'email_verified_at' => now(),
             'password'          => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'conversation_id'   => $request['correlationId'],
             'remember_token'    => Str::random(10),
         ];
 
