f2d5260a1f3c85cfa729ef4f8c1ff398b007603e
Allison Oliveira <aoliveira@nuveto.com.br>
2020-12-09 00:21:43 -0300

    changes

--- app/Http/Controllers/TwitterCallbackController.php
+++ app/Http/Controllers/TwitterCallbackController.php
@@ -54,6 +54,8 @@ class twitterCallbackController extends Controller
 
                 $sender_id = $events['message_create']['sender_id'];
 
+                $text = $events['message_create']['message_data']['text'];
+
                 $config = DB::table('conversation_configs')->where('channel', '=', 'twitter')->first();
 
                 $twitter_session = DB::table('twitter_conversations')->where('sender_id', $sender_id)->first();
@@ -111,7 +113,7 @@ class twitterCallbackController extends Controller
                                 $insert_params_twitter = [
                                     'tokenId'           => $create_session['tokenId'],
                                     'sender_id'         => $sender_id,
-                                    'text'              => $events['message_data']['text'],
+                                    'text'              => $text,
                                     'conversationId'    => $create_conversation['body']['id'],
                                     'farmId'            => $create_session['context']['farmId'],
                                     'payload'           => $request
@@ -171,7 +173,7 @@ class twitterCallbackController extends Controller
                             $insert_params_twitter = [
                                 'tokenId'           => $create_session['tokenId'],
                                 'sender_id'         => $sender_id,
-                                'text'              => $events['message_data']['text'],
+                                'text'              => $text,
                                 'conversationId'    => $create_conversation['body']['id'],
                                 'farmId'            => $create_session['context']['farmId'],
                                 'payload'           => $request
