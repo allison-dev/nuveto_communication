b58ad9cc326b9f27077092d473e1f891869a97a4
Allison Oliveira <aoliveira@nuveto.com.br>
2020-12-09 22:38:39 -0300

    chat/twitter finish

--- app/Http/Controllers/FivenineCallbackController.php
+++ app/Http/Controllers/FivenineCallbackController.php
@@ -3,16 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\DB;
+use App\Facades\ChatifyMessenger as Chatify;
 use Illuminate\Support\Str;
+use Faker\Factory as Faker;
 
 class fivenineCallbackController extends Controller
 {
     public function chatSession(Request $request)
     {
+        $faker = Faker::create();
+
         $insert_params = [
-            'name'              => 'teste API',
-            'email'             => 'teste@tes.com',
+            'name'              => $faker->name(),
+            'email'             => $faker->unique()->email(),
             'email_verified_at' => now(),
             'password'          => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'conversation_id'   => $request['correlationId'],
@@ -26,14 +31,48 @@ class fivenineCallbackController extends Controller
 
     public function chatCallback(Request $request)
     {
-        sendChatCallback($request);
+        // default variables
+        $error_msg = null;
 
-        return response()->json(['data' => $request]);
+        $from_id = DB::table('users')->where('conversation_id', '=', $request['correlationId'])->first();
+
+        if (!$error_msg) {
+            // send to database
+            $messageID = $request['correlationId'];
+            Chatify::newMessage([
+                'id' => (string) $messageID,
+                'type' => 'API',
+                'from_id' => (string) $from_id->id,
+                'to_id' => (string) $request['externalId'],
+                'body' => $request['text'],
+                'attachment' => '',
+            ]);
+
+            // fetch message to send it with the response
+            $messageData = Chatify::fetchMessage($messageID);
+        }
+
+        // send the response
+        return Response::json([
+            'status' => '200',
+            'error' => $error_msg ? 1 : 0,
+            'error_msg' => $error_msg,
+            'message' => Chatify::messageCard(@$messageData),
+            'tempID' => $request['messageId'],
+        ]);
     }
 
     public function chatTerminate(Request $request)
     {
-        // DB::table('users')->where('id', $request['correlationId'])->delete();
+        DB::table('users')->where('conversation_id', $request['correlationId'])->delete();
+        DB::table('conversation_sessions')->where('conversationId', '=', $request['correlationId'])->update(['terminate' => '1']);
+
+        return response()->json([], 204);
+    }
+
+    public function chatAccept(Request $request)
+    {
+        DB::table('users')->where('conversation_id', $request['correlationId'])->update(['name' => $request['displayName']]);
 
         return response()->json([], 204);
     }
