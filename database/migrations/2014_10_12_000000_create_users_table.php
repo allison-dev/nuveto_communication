e3df6fc27cf735abcd89264fe0af25666c148940
Allison Oliveira <aoliveira@nuveto.com.br>
2021-08-03 23:57:06 -0300

    clear code

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,18 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('conversation_id')->nullable();
             $table->string('login')->nullable();
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('usr_active')->nullable();
-            $table->string('activation_code')->nullable();
-            $table->string('priv_admin')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->string('conversation_id')->nullable();
-            $table->foreignId('current_team_id')->nullable();
-            $table->text('profile_photo_path')->nullable();
             $table->timestamps();
         });
     }
--- resources/views/pages/admin/home/index.blade.php
+++ resources/views/pages/admin/home/index.blade.php
@@ -8,66 +8,74 @@
 
 @section('content')
     <div class="row">
-        <div class="col-lg-6 col-sm-12">
-            <div class="card">
-                <div class="card-header border-0">
-                    <div class="d-flex justify-content-between">
-                        <h3 class="card-title"><b>{{ $chart_titles['line'] }}</b></h3>
+        @if ($line_chartjs)
+            <div class="col-lg-6 col-sm-12">
+                <div class="card">
+                    <div class="card-header border-0">
+                        <div class="d-flex justify-content-between">
+                            <h3 class="card-title"><b>{{ $chart_titles['line'] }}</b></h3>
+                        </div>
                     </div>
-                </div>
-                <div class="card-body">
-                    <div class="d-flex">
-                        <div class="position-relative">
-                            {!! $line_chartjs->render() !!}
+                    <div class="card-body">
+                        <div class="d-flex">
+                            <div class="position-relative">
+                                {!! $line_chartjs->render() !!}
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="col-lg-6 col-sm-12">
-            <div class="card">
-                <div class="card-header border-0">
-                    <div class="d-flex justify-content-between">
-                        <h3 class="card-title"><b>{{ $chart_titles['pie'] }}</b></h3>
+        @endif
+        @if ($pie_chartjs)
+            <div class="col-lg-6 col-sm-12">
+                <div class="card">
+                    <div class="card-header border-0">
+                        <div class="d-flex justify-content-between">
+                            <h3 class="card-title"><b>{{ $chart_titles['pie'] }}</b></h3>
+                        </div>
                     </div>
-                </div>
-                <div class="card-body">
-                    {!! $pie_chartjs->render() !!}
+                    <div class="card-body">
+                        {!! $pie_chartjs->render() !!}
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="col-lg-12 col-sm-12">
-            <div class="card">
-                <div class="card-header border-0">
-                    <div class="d-flex justify-content-between">
-                        <h3 class="card-title"><b>{{ $chart_titles['stacked'] }}</b></h3>
+        @endif
+        @if ($concurrent_chartjs)
+            <div class="col-lg-12 col-sm-12">
+                <div class="card">
+                    <div class="card-header border-0">
+                        <div class="d-flex justify-content-between">
+                            <h3 class="card-title"><b>{{ $chart_titles['concurrent'] }}</b></h3>
+                        </div>
                     </div>
-                </div>
-                <div class="card-body">
-                    <div class="d-flex">
-                        <div class="position-relative">
-                            {!! $stacked_chartjs->render() !!}
+                    <div class="card-body">
+                        <div class="d-flex">
+                            <div class="position-relative">
+                                {!! $concurrent_chartjs->render() !!}
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="col-lg-12 col-sm-12">
-            <div class="card">
-                <div class="card-header border-0">
-                    <div class="d-flex justify-content-between">
-                        <h3 class="card-title"><b>{{ $chart_titles['bar'] }}</b></h3>
+        @endif
+        @if ($bar_chartjs)
+            <div class="col-lg-12 col-sm-12">
+                <div class="card">
+                    <div class="card-header border-0">
+                        <div class="d-flex justify-content-between">
+                            <h3 class="card-title"><b>{{ $chart_titles['bar'] }}</b></h3>
+                        </div>
                     </div>
-                </div>
-                <div class="card-body">
-                    <div class="d-flex">
-                        <div class="position-relative">
-                            {!! $bar_chartjs->render() !!}
+                    <div class="card-body">
+                        <div class="d-flex">
+                            <div class="position-relative">
+                                {!! $bar_chartjs->render() !!}
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        @endif
     </div>
 @endsection
 @include('pages.admin.users.form.styles')
